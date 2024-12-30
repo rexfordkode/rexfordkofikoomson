b5d055bae95fbafdbc029b9306217a1baced077e
Rexfordkode <rexfordk.heatmap@gmail.com>
2024-12-30 03:54:26 +0000

    added a clear package

---
+++ index.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+const clearConsole = require("clear-any-console");
+clearConsole();
+console.log(`
+👋 Hi, I’m Rexford Koomson (@rexfordkode)
+🚀 Fullstack Developer | 🧠 AI Integration Enthusiast | 💻 Cloud & Microservices Architect
+
+👀 What I'm Into:
+Software Engineering: Crafting scalable solutions and optimizing system design.
+Backend Expertise: NodeJS, NestJS, TypeScript, AWS Lambda, Redis, GraphQL, and RESTful APIs.
+Frontend Flair: Building intuitive UIs with React, Vue, and Storyblok.
+Cloud & Infrastructure: Docker, AWS, Microservices, and serverless architectures.
+AI Integration: Exploring how to merge AI into modern applications.
+🌱 Currently Learning:
+Advanced Cloud Solutions: Diving deeper into AWS, Docker, and distributed systems, AI Integration.
+
+AI & Machine Learning: Integrating AI with microservices for smarter systems.
+
+💬 Let’s Collaborate On:
+Fullstack projects with NodeJS, NestJS, GraphQL, or RESTful APIs.
+Serverless architectures using AWS Lambda.
+Cutting-edge AI integrations for real-world applications.
+Building efficient microservices and system designs.
+
+📫 Reach Me:
+🐦 Twitter: @originalrexford
+  linkedin: @rexford-kofi-koomson/
+`);
